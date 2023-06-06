1ba1699e09d17af5c2f560b96387929db688f7b3
Kevin Llaca <127748914+kllaca@users.noreply.github.com>
2023-06-06 12:44:30 -0700

    Update admin_contacts.php

--- phpFiles/admin_contacts.php
+++ phpFiles/admin_contacts.php
@@ -1,21 +1,25 @@
 <?php
 
+//database connection details 
 @include 'config.php';
 
 session_start();
-
+//retrieve the admin id 
 $admin_id = $_SESSION['admin_id'];
 
+//if the admin id is not set we go to the log in page
 if(!isset($admin_id)){
    header('location:login.php');
 };
 
+//if delete is set we retrieve the delete id and executes the sql query
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
+   //this makes sure we delete the corresponding message 
    mysqli_query($conn, "DELETE FROM `message` WHERE id = '$delete_id'") or die('Query failed');
+   //the user is redirected at the admin contacts page 
    header('location:admin_contacts.php');
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -31,20 +35,21 @@ if(isset($_GET['delete'])){
 
 </head>
 <body>
-   
-<?php @include 'admin_header.php'; ?>
+
+<?php @include 'admin_header.php'; //we include the header?>
 
 <section class="messages">
 
    <h1 class="title">Messages</h1>
 
    <div class="box-container">
-
+      <!-- sql query to fetch all the messages from the tabe-->
       <?php
        $select_message = mysqli_query($conn, "SELECT * FROM `message`") or die('Query failed');
        if(mysqli_num_rows($select_message) > 0){
           while($fetch_message = mysqli_fetch_assoc($select_message)){
       ?>
+      <!-- for each message it create a div to display its information-->
       <div class="box">
          <p>User id : <span><?php echo $fetch_message['user_id']; ?></span> </p>
          <p>Name : <span><?php echo $fetch_message['name']; ?></span> </p>
@@ -53,6 +58,7 @@ if(isset($_GET['delete'])){
          <p>Message : <span><?php echo $fetch_message['message']; ?></span> </p>
          <a href="admin_contacts.php?delete=<?php echo $fetch_message['id']; ?>" onclick="return confirm('Delete this message?');" class="delete-btn">Delete</a>
       </div>
+      <!-- if there are no rows there are no messages to display-->
       <?php
          }
       }else{
@@ -62,18 +68,6 @@ if(isset($_GET['delete'])){
    </div>
 
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
 
 <script src="js/admin_script.js"></script>
 

65824bb6b76cbf90999c9cfeee8b032267179e11
Tea Malasi <127749217+tmalasi@users.noreply.github.com>
2023-06-07 07:02:30 -0700

    Update admin_users.php

--- phpFiles/admin_users.php
+++ phpFiles/admin_users.php
@@ -1,17 +1,22 @@
 <?php
 
+//database connection details 
 @include 'config.php';
 
 session_start();
 
+//retrieve the admin id 
 $admin_id = $_SESSION['admin_id'];
 
+//if the admin id is not set we go to the log in page
 if(!isset($admin_id)){
    header('location:login.php');
 };
 
+//if delete is set we retrieve the delete id and executes the sql query
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
+   //this makes sure we delete the corresponding message 
    mysqli_query($conn, "DELETE FROM `users` WHERE id = '$delete_id'") or die('Query failed');
    header('location:admin_users.php');
 }
@@ -32,7 +37,7 @@ if(isset($_GET['delete'])){
 </head>
 <body>
    
-<?php @include 'admin_header.php'; ?>
+<?php @include 'admin_header.php'; //we include the header?>
 
 <section class="users">
 
@@ -40,14 +45,18 @@ if(isset($_GET['delete'])){
 
    <div class="box-container">
       <?php
+      //retrieve all users' information from the users 
          $select_users = mysqli_query($conn, "SELECT * FROM `users`") or die('Query failed');
          if(mysqli_num_rows($select_users) > 0){
+            //f there is at least one row, a loop is started 
             while($fetch_users = mysqli_fetch_assoc($select_users)){
       ?>
       <div class="box">
+         <!--users information from the database using fetch users-->
          <p>ID : <span><?php echo $fetch_users['id']; ?></span></p>
          <p>Username : <span><?php echo $fetch_users['name']; ?></span></p>
          <p>Email : <span><?php echo $fetch_users['email']; ?></span></p>
+         <!--The user_type value is used to set the color of the displayed text dynamically.-->
          <p>Type: <span style="color:<?php if($fetch_users['user_type'] == 'admin'){ echo 'var(--orange)'; }; ?>"><?php echo $fetch_users['user_type']; ?></span></p>
          <a href="admin_users.php?delete=<?php echo $fetch_users['id']; ?>" onclick="return confirm('delete this user?');" class="delete-btn">delete</a>
          <a href="user_role.php?id=<?php echo $fetch_users['id']; ?>" class="change-role-btn">Change role</a>
@@ -60,18 +69,6 @@ if(isset($_GET['delete'])){
    </div>
 
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
 
 <script src="js/admin_script.js"></script>
 

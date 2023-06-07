a3403e36f49fb2887b29a76b59a3db56266c017d
XhesBaze <102756527+XhesBaze@users.noreply.github.com>
2023-06-07 21:25:31 +0200

    Update admin_orders.php

--- phpFiles/admin_orders.php
+++ phpFiles/admin_orders.php
@@ -33,10 +33,8 @@ if(isset($_GET['delete'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>dashboard</title>
 
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom admin css file link  -->
    <link rel="stylesheet" href="css/admin_style.css">
 
 </head>

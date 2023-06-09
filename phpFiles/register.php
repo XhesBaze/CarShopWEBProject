ea4046a74c0ed407174488be3fda74907bb6b379
XhesBaze <102756527+XhesBaze@users.noreply.github.com>
2023-06-09 18:13:51 +0200

    Update register.php

--- phpFiles/register.php
+++ phpFiles/register.php
@@ -1,57 +1,48 @@
 <?php
+include 'config.php';
+session_start();
 
-@include 'config.php'; //include connection to database
-session_start(); //starting a session
-
-
-if(isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])) // if a user id is set 
-{
-   header('location:home.php'); // redirect to home page
-	exit();
+if (isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])) {
+    header('Location: home.php');
+    exit();
 }
 
-if(isset($_SESSION['admin_id']) && !empty($_SESSION['admin_id'])) // if an admin id is set 
-{
-   header('location:admin_page.php'); // redirect to admin page
-	exit();
+if (isset($_SESSION['admin_id']) && !empty($_SESSION['admin_id'])) {
+    header('Location: admin_page.php');
+    exit();
 }
 
+if (isset($_POST['submit'])) {
+    $name1 = $_POST['name'];
+    $email1 = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $pass1 = md5($_POST['pass']);
+    $pass2 = $_POST['pass'];
+    $cpass1 = md5($_POST['cpass']);
+    $pattern = '/^(?=(?:.*\d){2})(?=.*[!@#$%^&*()\-=_+])[^\';"]{8,16}$/';
 
-if (isset($_POST['submit'])) // if the form is submitted
-{
-	
-	$name1 = $_POST['name']; //store the name submitted in the name1 variable
-	$email1 = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL); //storing the sanitized email in the email1 variable
-	$pass1 = md5($_POST['pass']);// storing the hashed password in the pass1 variable 
-	$pass2 = $_POST['pass']; //retrieving the not hashed password
-	$cpass1 = md5($_POST['cpass']); // storing the text for the confirmation password in the cpass1 variable
-        $pattern = '/^(?=(?:.*\d){2})(?=.*[!@#$%^&*()\-=_+])[^\';"]{8,16}$/'; // pattern for the password
+    $stmt1 = mysqli_prepare($conn, "SELECT * FROM `users` WHERE email = ?");
+    mysqli_stmt_bind_param($stmt1, "s", $email1);
+    mysqli_stmt_execute($stmt1);
+    $res = mysqli_stmt_get_result($stmt1);
 
-  $stmt1 = mysqli_prepare($conn, "SELECT * FROM `users` WHERE email = ?"); //query to select information from the user table, where the email matches the placeholder
-    mysqli_stmt_bind_param($stmt1, "s", $email1); // binding the parameters, a type string will be bounded,email,to its placeholder
-    mysqli_stmt_execute($stmt1); // executing the query 
-    $res = mysqli_stmt_get_result($stmt1); //saving the result o
-	
-    if (mysqli_num_rows($res) > 0 ) // if there is a result that is returned (email has been saved already)
-    {
-        $message[] = 'User already exists!'; 
+    if (mysqli_num_rows($res) > 0) {
+        $message[] = 'User already exists!';
     } else {
-	if ($pass1 != $cpass1) {
+        if ($pass1 != $cpass1) {
             $message[] = 'Passwords do not match!';
         } else if (!preg_match($pattern, $pass2)) {
             $message[] = "Password must be 8-16 characters, including 2 numeric values, and 1 symbol (excluding ';' and '\")";
-        }
-		else {
-            $stmt1 = mysqli_prepare($conn, "INSERT INTO `users` (name, email, password) VALUES (?, ?, ?)"); // preparing an INSERT query, three placeholders for name,email,password
-            mysqli_stmt_bind_param($stmt1, "sss", $name1 , $email1, $pass1); // three strings will be binded
-            mysqli_stmt_execute($stmt1);// executing the query(inserting the user)
+        } else {
+            $stmt2 = mysqli_prepare($conn, "INSERT INTO `users` (name, email, password) VALUES (?, ?, ?)");
+            mysqli_stmt_bind_param($stmt2, "sss", $name1, $email1, $pass1);
+            mysqli_stmt_execute($stmt2);
+            mysqli_stmt_close($stmt2);
             $message[] = 'Successful sign up!';
-            header('location:login.php');//redirect to login if the sign up is successful
+            header('Location: login.php');
             exit();
         }
     }
-	    mysqli_stmt_close($stmt1);
-
+    mysqli_stmt_close($stmt1);
 }
 
 ?>
@@ -60,45 +51,90 @@ if (isset($_POST['submit'])) // if the form is submitted
 <html lang="en">
 
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Sign up</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up</title>
 
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-   <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="css/signin.css">
 
 </head>
 
 <body>
 
-<?php
-//showing error message
-if (isset($message)) {
-    foreach ($message as $msg) {
-        echo '
-      <div class="message">
-         <span>' . $msg . '</span>
-      </div>
-      ';
+    <?php
+    if (isset($message)) {
+        foreach ($message as $msg) {
+            echo '
+            <div class="message">
+                <span>' . $msg . '</span>
+            </div>
+            ';
+        }
     }
-}
-?>
+    ?>
 
-<section class="form-container">
+    <img class="backg" src="images/backg.png">
+    <div class="container">
+        <div class="images">
+            <img src="images/signin.svg">
+        </div>
 
-   <form action="" method="post">
-      <h3>Sign up now</h3>
-      <input type="text" name="name" class="box" placeholder="Enter your username:" required>
-      <input type="email" name="email" class="box" placeholder="Enter your email:" required>
-      <input type="password" name="pass" class="box" placeholder="Enter your password:" required>
-      <input type="password" name="cpass" class="box" placeholder="Confirm your password:" required>
-      <input type="submit" class="btn" name="submit" value="Register now">
-      <p>Already have an account? <a href="login.php">Login now!</a></p>
-   </form>
+        <div class="form-container">
+            <form action="" method="post">
+                <img src="./images/hello.svg">
+                <h2> Sign up now </h2>
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Username</h5>
+                        <input class="input" type="text" name="name" required>
+                    </div>
+                </div>
 
-</section>
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Email</h5>
+                        <input class="input" type="email" name="email" required>
+                    </div>
+                </div>
 
+
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Password</h5>
+                        <input class="input" type="password" name="pass" required>
+                    </div>
+                </div>
+
+
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-check"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Confirm password</h5>
+                        <input class="input" type="password" name="cpass" required>
+                    </div>
+                </div>
+
+                <input type="submit" class="btn" name="submit" value="Sign up">
+                <p>Already have an account? <a href="login.php">Login now!</a></p>
+
+            </form>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="js/signin.js"></script>
 </body>
 
 </html>

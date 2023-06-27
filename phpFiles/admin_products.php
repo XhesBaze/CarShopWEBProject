b4d70b0de9893742528add076caeb0094a6884e8
Tea Malasi <127749217+tmalasi@users.noreply.github.com>
2023-06-27 10:44:17 -0700

    Update admin_products.php

--- phpFiles/admin_products.php
+++ phpFiles/admin_products.php
@@ -79,9 +79,8 @@ if(isset($_GET['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>products</title>
-
-
+   <title>Admin Products</title>
+   <link rel="icon" type="image/png" href="..\images\admin-logo.png"/>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
    <link rel="stylesheet" href="css/admin_style.css">
 
@@ -91,16 +90,21 @@ if(isset($_GET['delete'])){
 <?php @include 'admin_header.php'; //we include the header?>
 
 <section class="add-products">
-
-   <form action="" method="POST" enctype="multipart/form-data">
-      <h3>Add a new product</h3>
-      <input type="text" class="box" required placeholder="enter product name" name="name">
-      <input type="number" min="0" class="box" required placeholder="enter product price" name="price">
-      <textarea name="details" class="box" required placeholder="enter product details" cols="30" rows="10"></textarea>
-      <input type="file" accept="image/jpg, image/jpeg, image/png" required class="box" name="image">
-      <input type="submit" value="add product" name="add_product" class="btn">
-   </form>
-
+<div class="image-form">
+   <div class="image">
+      <img src="..\images\car-product.png" alt="Product Image example" class="product-image">
+</div>
+<div class="form">
+      <form action="" method="POST" enctype="multipart/form-data">
+         <h3>Add a new product</h3>
+         <input type="text" class="box" required placeholder="Enter product name" name="name">
+         <input type="number" min="0" class="box" required placeholder="Enter product price" name="price">
+         <textarea name="details" class="box" required placeholder="Enter product details" cols="30" rows="10"></textarea>
+         <input type="file" accept="image/jpg, image/jpeg, image/png" required class="box" name="image">
+         <input type="submit" value="Add product" name="add_product" class="btn">
+      </form>
+</div>
+   </div>
 </section>
 
 <section class="show-products">

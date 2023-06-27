b4db11ef1457b822b2a02bd721c938ec93a9aad6
Kevin Llaca <127748914+kllaca@users.noreply.github.com>
2023-06-27 13:41:28 -0700

    Update shop.php

--- phpFiles/shop.php
+++ phpFiles/shop.php
@@ -66,7 +66,7 @@ if(isset($_POST['add_to_cart'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Shop</title>
-
+<link rel="icon" type="image/png" href="..\images\car-logo.jpg"/>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
    <link rel="stylesheet" href="css/style.css">
 

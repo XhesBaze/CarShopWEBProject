6fce5432fa02049f6865269b554264ebfef3453d
Kevin Llaca <127748914+kllaca@users.noreply.github.com>
2023-06-27 13:34:18 -0700

    Update register.php

--- phpFiles/register.php
+++ phpFiles/register.php
@@ -55,7 +55,7 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign up</title>
-
+<link rel="icon" type="image/png" href="..\images\car-logo.jpg"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="css/signin.css">
 

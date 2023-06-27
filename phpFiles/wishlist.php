fddc2e847ce9160722c9c257a349e2fcd099c934
Kevin Llaca <127748914+kllaca@users.noreply.github.com>
2023-06-27 13:29:29 -0700

    Update wishlist.php

--- phpFiles/wishlist.php
+++ phpFiles/wishlist.php
@@ -56,7 +56,7 @@ if(isset($_GET['delete_all'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Wishlist</title>
-
+<link rel="icon" type="image/png" href="..\images\car-logo.jpg"/>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
    <link rel="stylesheet" href="css/style.css">
 
@@ -103,7 +103,7 @@ if(isset($_GET['delete_all'])){
     ?>
     </div>
 </section>
-
+<?php @include 'footer.php'; ?>
 <script src="js/script.js"></script>
 
 </body>

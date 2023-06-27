f21f72a792ecad268c15d25392d437f274b7dc83
Tea Malasi <127749217+tmalasi@users.noreply.github.com>
2023-06-27 10:43:26 -0700

    Update admin_page.php

--- phpFiles/admin_page.php
+++ phpFiles/admin_page.php
@@ -21,20 +21,20 @@ if(!isset($admin_id)){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Dashboard</title>
-
+   <title>Admin Page</title>
+   <link rel="icon" type="image/png" href="..\images\admin-logo.png"/>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <link rel="stylesheet" href="css/admin_style.css">
 
 </head>
 <body>
-   
+    
 <?php @include 'admin_header.php'; //we include the header?>
 
 <section class="dashboard">
 
-   <h1 class="title">Dashboard</h1>
+   <h1 class="title">General information</h1>
 
    <div class="box-container">
 

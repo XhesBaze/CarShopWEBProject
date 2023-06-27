a7626ea25f7ea4157a383b9e8a48a5c05ed715d4
anteatoska <129380461+anteatoska@users.noreply.github.com>
2023-06-27 22:38:20 +0200

    Update contact.php

--- phpFiles/contact.php
+++ phpFiles/contact.php
@@ -1,78 +1,140 @@
 <?php
-//connection with the database
-@include 'config.php';
+include 'config.php';
 
 session_start();
 
-//saves in session the user id
 $user_id = $_SESSION['user_id'];
 
-//if id is not set, it redirects to login
-if(!isset($user_id)){
-   header('location:login.php');
-};
-
-//if send option is selected, name, email, number and message are saved from the database to session
-if(isset($_POST['send'])){
-
-    $name = mysqli_real_escape_string($conn, $_POST['name']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $number = mysqli_real_escape_string($conn, $_POST['number']);
-    $msg = mysqli_real_escape_string($conn, $_POST['message']);
-
-//it retrieves the message of the user based on his name, email and number
-    $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE name = '$name' AND email = '$email' AND number = '$number' AND message = '$msg'") or die('query failed');
-//if the user has written even one row, it means the message has been sent already
-//otherwise it inserts the meesage
-    if(mysqli_num_rows($select_message) > 0){
-        $message[] = 'message sent already!';
-    }else{
-        mysqli_query($conn, "INSERT INTO `message`(user_id, name, email, number, message) VALUES('$user_id', '$name', '$email', '$number', '$msg')") or die('query failed');
-        $message[] = 'message sent successfully!';
-    }
-
+if (!isset($user_id)) {
+    header('Location: login.php');
+    exit(); // Terminate the script after redirecting
 }
 
+$message = array(); // Initialize the $message array
+
+if (isset($_POST['send'])) {
+    // Check if the required form fields are filled
+    if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['number']) || empty($_POST['message'])) {
+        $message[] = 'Please fill in all the fields.';
+    } else {
+        // Sanitize user input to prevent SQL injection
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $number = mysqli_real_escape_string($conn, $_POST['number']);
+        $msg = mysqli_real_escape_string($conn, $_POST['message']);
+
+        // Check if the message has already been sent
+        $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE name = '$name' AND email = '$email' AND number = '$number' AND message = '$msg'") or die('Query failed');
+
+        if (mysqli_num_rows($select_message) > 0) {
+            $message[] = 'Message already sent!';
+        } else {
+            // Insert the message into the database
+            $insert_message = mysqli_query($conn, "INSERT INTO `message` (user_id, name, email, number, message) VALUES ('$user_id', '$name', '$email', '$number', '$msg')") or die('Query failed');
+
+            if ($insert_message) {
+                $message[] = 'Message sent successfully!';
+            } else {
+                $message[] = 'Failed to send the message. Please try again.';
+            }
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>contact</title>
-<link rel="icon" type="image/png" href="..\images\car-logo.jpg"/>
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-   <link rel="stylesheet" href="css/style.css">
-
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="css/contact.css" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-   
-<?php @include 'header.php'; ?>
 
-<section class="heading">
-    <h3>contact us</h3>
-    <p> <a href="home.php">home</a> / contact </p>
-</section>
+    <?php @include'header.php'?>;
 
-<section class="contact">
+    <div class="container-1">
+        <span class="big-circle"></span>
+        <img src="images/shape.png" class="square" alt="" />
+        <div class="form">
+            <div class="contact-info">
+                <h3 class="title">Get in touch with us!</h3>
+                <p class="text">
+                    We would love to hear more from you! Here are some other options for you to communicate with us.
+                </p>
 
-    <form action="" method="POST">
-        <h3>send us message!</h3>
-        <input type="text" name="name" placeholder="enter your name" class="box" required> 
-        <input type="email" name="email" placeholder="enter your email" class="box" required>
-        <input type="number" name="number" placeholder="enter your number" class="box" required>
-        <textarea name="message" class="box" placeholder="enter your message" required cols="30" rows="10"></textarea>
-        <input type="submit" value="send message" name="send" class="btn">
-    </form>
+                <div class="info">
+                    <div class="information">
+                        <img src="images/location.png" class="icon" alt="" />
+                        <p>92 Tirana, Albania</p>
+                    </div>
+                    <div class="information">
+                        <img src="images/email.png" class="icon" alt="" />
+                        <p>onthego@gmail.com</p>
+                    </div>
+                    <div class="information">
+                        <img src="images/phone.png" class="icon" alt="" />
+                        <p>+355691213134</p>
+                    </div>
+                </div>
 
-</section>
+                <div class="social-media">
+                    <p>Connect with us </p>
+                    <div class="social-icons">
+                        <a href="#">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
 
-<?php @include 'footer.php'; ?>
+            <div class="contact-form">
+                <span class="circle one"></span>
+                <span class="circle two"></span>
 
-<script src="js/script.js"></script>
+                <form action="" method="POST" autocomplete="off">
+                    <h3 class="title">Contact us</h3>
+                    <div class="input-container">
+                        <input type="text" name="name" class="input" required />
+                        <label for="">Username</label>
+                        <span>Username</span>
+                    </div>
+                    <div class="input-container">
+                        <input type="email" name="email" class="input" required />
+                        <label for="">Email</label>
+                        <span>Email</span>
+                    </div>
+                    <div class="input-container">
+                        <input type="tel" name="number" class="input" required />
+                        <label for="">Phone</label>
+                        <span>Phone</span>
+                    </div>
+                    <div class="input-container textarea">
+                        <textarea name="message" class="input" required></textarea>
+                        <label for="">Message</label>
+                        <span>Message</span>
+                    </div>
+                    <input type="submit" name="send" value="Send" class="btn" />
+                </form>
+            </div>
+        </div>
+    </div>
 
+    <script src="js/contact.js"></script>
 </body>
+
 </html>

7af57b30a2d034207466886028ab3a426ea556aa
Kevin Llaca <127748914+kllaca@users.noreply.github.com>
2023-06-27 13:32:51 -0700

    Update checkout.php

--- phpFiles/checkout.php
+++ phpFiles/checkout.php
@@ -84,7 +84,7 @@ if (isset($_POST['order']))  //if we retrieve this action from the form
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>checkout</title>
-
+<link rel="icon" type="image/png" href="..\images\car-logo.jpg"/>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <link rel="stylesheet" href="css/style.css">

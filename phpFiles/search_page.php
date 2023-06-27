d9ae1a71a701b360aa48029eb8a3cd210a324965
Kevin Llaca <127748914+kllaca@users.noreply.github.com>
2023-06-27 13:28:17 -0700

    Update search_page.php

--- phpFiles/search_page.php
+++ phpFiles/search_page.php
@@ -45,7 +45,7 @@ if(isset($_POST['add_to_cart'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Search page</title>
-
+<link rel="icon" type="image/png" href="..\images\car-logo.jpg"/>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <link rel="stylesheet" href="css/style.css">
@@ -102,7 +102,7 @@ if(isset($_POST['add_to_cart'])){
    </div>
 
 </section>
-
+<?php @include 'footer.php'; ?>
 <script src="js/script.js"></script>
 
 </body>

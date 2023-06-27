e9212cef965bc93ef45d15b5e0d7abb6ea17c892
Kevin Llaca <127748914+kllaca@users.noreply.github.com>
2023-06-27 13:26:34 -0700

    Update home.php

--- phpFiles/home.php
+++ phpFiles/home.php
@@ -10,7 +10,7 @@ session_start(); //start session
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Home</title>
-
+<link rel="icon" type="image/png" href="..\images\car-logo.jpg"/>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
    <link rel="stylesheet" href="css/style.css">
 
@@ -177,7 +177,7 @@ session_start(); //start session
    </div>
 
 </section>
-
+<?php @include 'footer.php'; ?>
 <script>
    function showSlide(slideNumber) {
       const slides = document.querySelectorAll('.slider img');

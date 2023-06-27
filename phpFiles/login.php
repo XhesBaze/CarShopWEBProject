94ac21c17b10bd6e3936874476df4509282b5588
Kevin Llaca <127748914+kllaca@users.noreply.github.com>
2023-06-27 13:33:42 -0700

    Update login.php

--- phpFiles/login.php
+++ phpFiles/login.php
@@ -56,7 +56,7 @@ if (isset($_POST['submit'])) { //if the form has been submitted
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-
+<link rel="icon" type="image/png" href="..\images\car-logo.jpg"/>
     <link rel="stylesheet" type="text/css" href="css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
